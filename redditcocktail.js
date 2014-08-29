e79644a17e195138a543f4f53c97a307b3b44c1b
redditcocktail <reddit.cocktail@gmail.com>
2014-08-29 17:28:32 +0200

    Update redditcocktail.js

--- redditcocktail.js
+++ redditcocktail.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    memory.recover();
     drinkCocktail({
         'name': 'test',
         'ingredients': [
@@ -11,56 +12,184 @@ $(document).ready(function() {
 
 
 var drinkCocktail = function(cocktail) {
+    var UNIT = 5;
     $('#home-page').css('display', 'none');
     $('#cokctail-editor').css('display', 'none');
-    var tmp = function() {
-        addLink({
-            'domain': 'farm6.staticflickr.com', 
-            'banned_by': null,
-            'media_embed': {},
-            'subreddit': 'CityPorn',
-            'selftext_html': null,
-            'selftext': '',
-            'likes': null,
-            'secure_media': null,
-            'link_flair_text': null,
-            'id': '2evcxo',
-            'gilded': 0,
-            'secure_media_embed': {},
-            'clicked': false,
-            'stickied': false,
-            'author': 'mojave955',
-            'media': null,
-            'score': 50,
-            'approved_by': null,
-            'over_18': false,
-            'hidden': false,
-            'thumbnail': 'http://b.thumbs.redditmedia.com/65jJnvhn9ZC-66EYLpigbSvvRtG1IIwvO0Ko2zDXt1Q.jpg',
-            'subreddit_id': 't5_2scjs',
-            'edited': false,
-            'link_flair_css_class': null,
-            'author_flair_css_class': 'Camera',
-            'downs': 0,
-            'saved': false,
-            'is_self': false,
-            'permalink': '/r/CityPorn/comments/2evcxo/view_from_my_grandparents_house_in_daegu_south/',
-            'name': 't3_2evcxo',
-            'created': 1409298046.0,
-            'url': 'https://farm6.staticflickr.com/5555/15046967545_8d4a39b76e_h.jpg',
-            'author_flair_text': null,
-            'title': 'View from my grandparents\' house in Daegu, South Korea [2048 x 1365]',
-            'created_utc': 1409269246.0,
-            'ups': 50,
-            'num_comments': 12,
-            'visited': false,
-            'num_reports': null,
-            'distinguished': null
-        });
+    var scrapers = [],
+        $lastLink = null,
+        loadingLinks = false;
+    for (var i = 0; i < cocktail.ingredients.length; i++)
+        scrapers.push(new SubredditScraper(cocktail.ingredients[i].subreddit));
+    var addMoreLinks = function(callback) {
+        loadingLinks = true;
+        var newLinks = [];
+        for (var i = 0; i < scrapers.length; i++) {
+            (function(i) { // Closure over 'i'.
+            newLinks.push(undefined);
+            scrapers[i].scrape(
+                UNIT * Number(cocktail.ingredients[i].amount),
+                function(links) {
+                    if (links === []) { // The scraper is exhausted.
+                        newLinks[i] = null;
+                    }
+                    else {
+                        newLinks[i] = [];
+                        for (var j = 0; j < links.length; j++) {
+                            newLinks[i].push(links[j]);
+                        }
+                    }
+                    // If all scrapers have yielded a result:
+                    if (newLinks.indexOf(undefined) === -1) addLinks();
+                },
+                function() {
+                    newLinks[i] = null;
+                    // If all scrapers have yielded a result:
+                    if (newLinks.indexOf(undefined) === -1) addLinks();
+                }
+            );
+            })(i); // End of closure over 'i'.
+        }
+        var addLinks = function() {
+            // Create small bundles of links, with the same amount of links
+            // per subreddit per bundle.
+            var bundles = [];
+            for (var __ = 0; __ < UNIT; __++) {
+                bundles.push([]);
+                for (var i = 0; i < newLinks.length; i++) {
+                    if (newLinks[i] === null) continue;
+                    var amount = Number(cocktail.ingredients[i].amount);
+                    for (var _ = 0; _ < amount; _++) {
+                        bundles[bundles.length-1].push(newLinks[i].shift());
+                    }
+                }
+            }
+            // For each bundle, shuffle it and display its links.
+            while (bundles.length > 0) {
+                var bundle = bundles.shift();
+                bundle = shuffleArray(bundle);
+                while (bundle.length > 0)
+                    $lastLink = addLink(bundle.shift());
+            }
+            loadingLinks = false;
+        };
     };
-    tmp();
-    tmp();
-    tmp();
-    tmp();
+    addMoreLinks();
+    $(window).scroll(function() {
+        if (loadingLinks) return;
+        if ($lastLink === null) return;
+        var windowBottom = $(window).scrollTop() + $(window).height(),
+            loadMoreLinksLimit = $(document).height() - 20*$lastLink.height();
+        if (windowBottom >= loadMoreLinksLimit) {
+            addMoreLinks();
+        }
+    });
+};
+
+
+var SubredditScraper = function(subreddit) {
+    this.subreddit = subreddit;
+    this.after = '';
+    this.top = 'day';
+    this.topScore = 0;
+    this.links = [];
+    this.requestURL = '';
+    this.idsOfYieldedLinks = [];
+    this.LIMIT = 50;
+    // Links the scores of which are below QUALITY multiplied against
+    // the best score in the current top so far are discarded.
+    // For example, if the scraper is browsing the 'week' top of its
+    // subreddit, and the highest score it has seen in this 'week' top
+    // is 2500, then links of score lesser than QUALITY*2500 will not be
+    // yielded.
+    this.QUALITY = .10;
+};
+SubredditScraper.prototype.buildRequestURL = function() {
+    this.requestURL = [
+        'http://www.reddit.com/r/',
+        this.subreddit,
+        '/top.json?limit=',
+        this.LIMIT,
+        '&after=',
+        this.after,
+        '&t=',
+        this.top,
+        '&jsonp=?'
+    ].join('');
+};
+SubredditScraper.prototype.changeTop = function() {
+    if (this.top === 'day') this.top = 'week';
+    else if (this.top === 'week') this.top = 'month';
+    else if (this.top === 'month') this.top = 'all';
+    this.after = '';
+    this.topScore = 0;
+};
+SubredditScraper.prototype.scrape = function(amount, success, error) {
+    if (amount === undefined) amount = this.LIMIT;
+    if (success === undefined) success = function(links) {};
+    if (error === undefined) error = function(links) {};
+    if (this.after === null) { // The subreddit is exhausted.
+        success([]);
+        return;
+    }
+    this.buildRequestURL();
+    var scraper = this;
+    $.getJSON(this.requestURL, function(json) {
+        if (json.data.children === undefined) {
+            error();
+            return;
+        }
+        for (var i = 0; i < json.data.children.length; i++) {
+            // Check if this link has already been seen.
+            if (scraper.idsOfYieldedLinks.indexOf(
+                        json.data.children[i].data.id) !== -1 ||
+                    memory.remembers(json.data.children[i].data.id)) {
+                continue;
+            }
+            // Update the top score of the current top if it needs be.
+            if (Number(json.data.children[i].data.score) > scraper.topScore) {
+                scraper.topScore = Number(json.data.children[i].data.score);
+            }
+            // Change top if the current link (and consequently all the
+            // following links) does not have a high enough score.
+            if (Number(json.data.children[i].data.score) < scraper.QUALITY*
+                        scraper.topScore &&
+                    scraper.top !== 'all') {
+                scraper.changeTop();
+                scraper.scrape(amount, success, error);
+                return;
+            }
+            // If the current link has passed the precedent test, then put
+            // it among the links to be yielded, and register it id in order
+            // not to yield it twice.
+            scraper.links.push(json.data.children[i].data);
+            scraper.idsOfYieldedLinks.push(json.data.children[i].data.id);
+            if (scraper.links.length === amount) {
+                // When enough links have been scraped, send them, and stop
+                // the scraping.
+                scraper.after = json.data.children[i].data.name;
+                success(scraper.links);
+                scraper.links = [];
+                return;
+            }
+        }
+        // If the code goes down there, then not enough links have been yielded.
+        if (json.data.after !== null) {
+            // If there are still links to scrape in the current top.
+            scraper.after = json.data.after;
+            scraper.scrape(amount, success, error);
+        }
+        else if (scraper.top !== 'all') {
+            // If there are not links to scrape in the current top anymore.
+            scraper.changeTop();
+            scraper.scrape(amount, success, error);
+        }
+        else {
+            // If there are not links to scrape at all anymore.
+            scraper.after = null;
+            success(scraper.links);
+            scraper.links = [];
+        }
+    });
 };
 
 
@@ -70,7 +199,7 @@ var addLink = function(link) {
         'href': link.url,
         'target': '_blank'
     });
-    $('<img>', {'src': link.thumbnail}).appendTo($link);
+    if (!link.is_self) $('<img>', {'src': link.thumbnail}).appendTo($link);
     var $description = $('<div>', {'class': 'link-description'});
     $('<h2>').text(link.title).appendTo($description);
     $('<p>').text('Author: ' + link.author).appendTo($description);
@@ -86,6 +215,7 @@ var addLink = function(link) {
     ].join('')).appendTo($description);
     $description.appendTo($link);
     $link.appendTo($('#cocktail-drinker'));
+    return $link;
 };
 
 
@@ -107,4 +237,17 @@ var timeSince = function(date) {
     if (interval > 1)
         return interval + ' minutes';
     return Math.floor(seconds) + ' seconds';
+}
+
+/* Randomize array element order in-place.
+ * Using Fisher-Yates shuffle algorithm.
+ */
+var shuffleArray = function(a) {
+    for (var i = a.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    return a;
 }
