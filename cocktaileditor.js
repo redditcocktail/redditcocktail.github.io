17e8a68ebc1caf1a099edf5c02841eadeb17c908
redditcocktail <reddit.cocktail@gmail.com>
2014-08-29 21:09:06 +0200

    Update cocktaileditor.js

--- cocktaileditor.js
+++ cocktaileditor.js
@@ -1,3 +1,15 @@
 var cocktaileditor = (function() {
 
+
+var startCocktailEditor = function() {
+    $('#home-page').css('display', 'none');
+    $('#cocktail-drinker').css('display', 'none');
+    $('#cocktail-editor').css('display', 'block');
+};
+
+
+return {
+    startCocktailEditor: startCocktailEditor
+};
+
 })();
