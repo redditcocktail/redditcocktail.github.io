162b0fba6faf368be1a52b7cd512f166c441a06a
redditcocktail <reddit.cocktail@gmail.com>
2014-09-04 20:43:59 +0200

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -6,25 +6,6 @@ var startHomePage = function() {
         localStorage.setItem('cocktails', JSON.stringify({}));
     $('#home-page').css('display', 'block');
     $('#cocktail-drinker').css('display', 'none');
-    $('#cocktail-editor').css('display', 'none');
-    $('#cocktail-select').empty();
-    var cocktails = JSON.parse(localStorage.getItem('cocktails'));
-    $('<option>', {'value': ''})
-        .attr('selected', 'selected')
-        .appendTo($('#cocktail-select'));
-    for (var cocktail in cocktails) {
-        $('<option>', {'value': cocktail})
-            .text(cocktails[cocktail].name)
-            .appendTo($('#cocktail-select'));
-    }
-    $('#cocktail-select').change(function() {
-        if ($('#cocktail-select option:selected').length !== 1) return;
-        if ($('#cocktail-select').val() === '') return;
-        cocktaildrinker.drinkCocktail(cocktails[$('#cocktail-select').val()]);
-    });
-    $('#create-edit-cocktail-button').click(function() {
-        cocktaileditor.startCocktailEditor();
-    });
 };
 
 
